137447a3e289af6c6133e6bc20320286211e0d97
Ilya <ilyaro@gmail.com>
2024-10-01 15:35:19 -0700

    staged commit

--- src/Components/building-blocks/navitems.js
+++ src/Components/building-blocks/navitems.js
@@ -9,11 +9,11 @@ const navItems = [
       path: "/ourstory",
       component: Story,
       meta: {
-        title: "Our Story",
+        title: "Le Petit Croissant",
         description: "Learn more about the history and background of Le Petit Croissant.",
         ogTitle: "Our Story - Le Petit Croissant",
         ogDescription: "Discover the legacy of our restaurant.",
-        ogImage: "%PUBLIC_URL%/og-contact.jpg"
+        ogImage: "/og-contact.jpg"
       }
     },
     {
@@ -21,11 +21,11 @@ const navItems = [
       path: "/foodmenu",
       component: Foodmenu,
       meta: {
-        title: "Menu",
+        title: "Le Petit Croissant",
         description: "Explore our delicious menu offerings at Le Petit Croissant.",
         ogTitle: "Menu - Le Petit Croissant",
         ogDescription: "Check out our various dishes and offerings.",
-        ogImage: "%PUBLIC_URL%/og-foodmenu.jpg"
+        ogImage: "/og-foodmenu.jpg"
       }
     },
     {
@@ -33,11 +33,11 @@ const navItems = [
       path: "/order",
       component: Order,
       meta: {
-        title: "Order",
+        title: "Le Petit Croissant",
         description: "Place your order online at Le Petit Croissant.",
         ogTitle: "Order - Le Petit Croissant",
         ogDescription: "Order your favorite dishes from Le Petit Croissant online.",
-        ogImage: "%PUBLIC_URL%/og-delivery.jpg"
+        ogImage: "/og-delivery.jpg"
       }
     },
     {
@@ -45,11 +45,11 @@ const navItems = [
       path: "/contact",
       component: Contact,
       meta: {
-        title: "Contact Us",
+        title: "Le Petit Croissant",
         description: "Get in touch with us at Le Petit Croissant.",
         ogTitle: "Contact Us - Le Petit Croissant",
         ogDescription: "We'd love to hear from you. Contact us today!",
-        ogImage: "%PUBLIC_URL%/og-contact.jpg"
+        ogImage: "og-contact.jpg"
       }
     }
   ];
